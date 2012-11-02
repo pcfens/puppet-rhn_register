3a615922bc7b8072e09b431472089ea3bc12071e
Phil Fenstermacher <pcfens@wm.edu>
2012-11-02 11:23:50 -0400

    Add tests

---
+++ tests/init.pp
@@ -0,0 +1,5 @@
+class { 'rhn_register':
+  sslca         => '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
+  serverurl     => 'https://satellite.myorg.cmo/XMLRPC',
+  activationkey => '1-1d6cfafaa90a972e7d3c2743e19f18cc',
+}
