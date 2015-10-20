3586998bbf6aca0a5b5d1df5a2fc9cacacf4b547
Phil Fenstermacher <pcfens@wm.edu>
2015-10-20 14:47:03 -0400

    Add tests around using subscription-manager

--- spec/classes/rhn_register_spec.rb
+++ spec/classes/rhn_register_spec.rb
@@ -7,32 +7,79 @@ describe 'rhn_register', :type => 'class' do
       :operatingsystem => 'RedHat'
     } end
 
-    context "with a username and password supplied" do
-      let :params do {
-        :username => 'test',
-        :password => 'test',
-      } end
+    describe 'using classic registration' do
+      context "with a username and password supplied" do
+        let :params do {
+          :username => 'test',
+          :password => 'test',
+        } end
 
-      it { should contain_exec('register_with_rhn').with(
-        :command => '/usr/sbin/rhnreg_ks --username=test --password=test'
-      ) }
+        it { should contain_exec('register_with_rhn').with(
+          :command => '/usr/sbin/rhnreg_ks --username=test --password=test'
+        ) }
+      end
+
+      context "with a server url defined" do
+        let :params do {
+          :username => 'test',
+          :password => 'test',
+          :serverurl => 'http://example.com/XMLRPC',
+        } end
+
+        it { should contain_exec('register_with_rhn').with(
+          :command => '/usr/sbin/rhnreg_ks --username=test --password=test --serverUrl=http://example.com/XMLRPC'
+        ) }
+      end
+
+      context "with some additional bool options set" do
+        let :params do {
+          :username => 'test',
+          :password => 'test',
+          :rhnsd    => false,
+          :virtinfo => false,
+        } end
+
+        it { should contain_exec('register_with_rhn').with(
+          :command => '/usr/sbin/rhnreg_ks --username=test --password=test --novirtinfo --norhnsd'
+        ) }
+      end
+
+      context "without a username/password or activation key supplied" do
+        it { expect { should raise_error(Puppet::Error, /Either an activation key or username\/password is required to register/) }}
+      end
     end
 
-    context "with a server url defined" do
-      let :params do {
-        :username => 'test',
-        :password => 'test',
-        :serverurl => 'http://example.com/XMLRPC',
-      } end
+    describe 'using subscription-manager' do
+      context "with a username and password supplied" do
+        let :params do {
+          :use_classic => false,
+          :username => 'test',
+          :password => 'test',
+        } end
 
-      it { should contain_exec('register_with_rhn').with(
-        :command => '/usr/sbin/rhnreg_ks --username=test --password=test --serverUrl=http://example.com/XMLRPC'
-      ) }
+        it { should contain_exec('register_with_rhn').with(
+          :command => '/usr/bin/subscription-manager register --username=test --password=test --auto-attach'
+        ) }
+      end
+
+      context "with a server url defined" do
+        let :params do {
+          :use_classic => false,
+          :username => 'test',
+          :password => 'test',
+          :serverurl => 'http://example.com/XMLRPC',
+        } end
+
+        it { should contain_exec('register_with_rhn').with(
+          :command => '/usr/bin/subscription-manager register --username=test --password=test --serverurl=http://example.com/XMLRPC --auto-attach'
+        ) }
+      end
+
+      context "without a username/password or activation key supplied" do
+        it { expect { should raise_error(Puppet::Error, /Either an activation key or username\/password is required to register/) }}
+      end
     end
 
-    context "without a username/password or activation key supplied" do
-      it { expect { should raise_error(Puppet::Error, /Either an activation key or username\/password is required to register/) }}
-    end
   end
 
   context "On an Ubuntu system" do
